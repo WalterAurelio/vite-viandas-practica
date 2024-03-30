5330df91b28c5f61fa29a07f0e9bdf88768e190f
Aurelio Gareca <aurelio.gareca.94@hotmail.com>
2024-03-29 22:45:02 -0300

    Trabajando en la responsividad del carrito de compras...

--- src/components/ArticuloCarrito.jsx
+++ src/components/ArticuloCarrito.jsx
@@ -10,8 +10,22 @@ function ArticuloCarrito({ articulo }) {
   const { eliminarDeCarrito } = useContext(CarritoContext);
 
   return (
+    // <div className="articulo-carrito">
+    //   <img className="image" src={articulo.image} alt="img_compra" />
+    //   <div className="info-pedido">
+    //     <p><b>{articulo.name}</b></p>
+    //     <p className="cantidad-pedido"><b>Cantidad</b> (1)</p>
+    //     <p className="subtotal-pedido"><b>Subtotal</b> {ARPesos.format(articulo.price)}</p>
+    //     <button type="button" className="boton-eliminar"
+    //       onClick={() => { eliminarDeCarrito(articulo) }}
+    //     >
+    //       Eliminar
+    //     </button>
+    //   </div>
+    // </div>
+
     <div className="articulo-carrito">
-      <img className="image" src={articulo.image} alt="img_compra" height={150} />
+      <img className="image" src={articulo.image} alt="img_compra" />
       <div className="info-pedido">
         <p><b>{articulo.name}</b></p>
         <p className="cantidad-pedido"><b>Cantidad</b> (1)</p>
--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -1,15 +1,26 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import ArticuloCarrito from "./ArticuloCarrito";
+
+let ARPesos = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
+
+const root = document.querySelector(':root');
 
 function Carrito({ listaCarrito }) {
   const [isActive, setIsActive] = useState(false);
   const carritoVacio = listaCarrito.length < 1;
-  const ref = useRef(null);
+  let precioTotal = 0;
+
+  listaCarrito.forEach(producto => {
+    precioTotal = precioTotal + producto.price;
+  });
 
   return (
     <>
       <button
-        className='boton-carrito'
+        className={`boton-carrito ${isActive ? 'is-active' : ''} ${listaCarrito.length > 4 ? 'gt-4' : ''}`.trimEnd()}
         onClick={() => { setIsActive(!isActive) }}
       >
         Carrito
@@ -17,7 +28,7 @@ function Carrito({ listaCarrito }) {
       <div className={`carrito ${isActive ? 'is-active' : ''} ${carritoVacio ? 'vacio' : ''}`.trimEnd()}>
         {
           carritoVacio &&
-          <p className="mensaje vacio">El carrito se encuentra vacío.</p>
+          <p className='mensaje vacio'>El carrito se encuentra vacío.</p>
         }
         <div>
           {
@@ -29,9 +40,14 @@ function Carrito({ listaCarrito }) {
             )
           }
         </div>
-        <div className={`container-total ${isActive ? 'is-active' : ''}`}>
-          <p>Total: </p>
-          <p>$20,000.00</p>
+        <div className={`container-total ${isActive ? 'is-active' : ''}`.trimEnd()}>
+          {
+            !carritoVacio &&
+            <>
+              <p>Total: </p>
+              <p>{ARPesos.format(precioTotal)}</p>
+            </>
+          }
         </div>
       </div>
     </>
