46c3cee91a924ef26b43533b3de90cbe1201d037
Aurelio Gareca <aurelio.gareca.94@hotmail.com>
2024-04-06 15:52:12 -0300

    Se les dieron estilos a las secciones de contacto y footer.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import { CarritoContext } from './contexts/CarritoContext';
 import { EntradaContext } from './contexts/EntradaContext';
 import { v4 as uuidv4 } from 'uuid';
 import HamburgerMenu from './components/HamburgerMenu';
+import { BsInstagram, BsTwitterX, BsYoutube } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
 
 let listaPlatos = platosJson.platos;
 
@@ -86,8 +88,22 @@ function App() {
           <div className='info-contacto'>
             <h2>Viandas Viking</h2>
 
+            <div className='container-redessociales'>
+              <div className='red-social fb'>
+                <FaFacebookF className='icono' />
+              </div>
+              <div className='red-social ig'>
+                <BsInstagram className='icono' />
+              </div>
+              <div className='red-social tw'>
+                <BsTwitterX className='icono' />
+              </div>
+              <div className='red-social yt'>
+                <BsYoutube className='icono' />
+              </div>
+            </div>
+
             <div className='container-lista'>
-              <h3>Info VK</h3>
               <ul className='lista'>
                 <li><a href='#'>Menú</a></li>
                 <li><a href='#'>Restaurantes</a></li>
@@ -97,7 +113,6 @@ function App() {
             </div>
 
             <div className='container-lista'>
-              <h3>Información</h3>
               <ul className='lista'>
                 <li><a href='#'>Contáctanos</a></li>
                 <li><a href='#'>Empleo</a></li>
@@ -110,37 +125,6 @@ function App() {
 
         <footer className='container-footer'>
           <p><b>VK®2024</b> Todos los derechos reservados.</p>
-
-          <div className='container-redessociales'>
-            <div className='red-social fb'>
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
-              </svg>
-            </div>
-            <div className='red-social ig'>
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
-                <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-                <path d="M16.5 7.5l0 .01" />
-              </svg>
-            </div>
-            <div className='red-social tw'>
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-x" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
-                <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
-              </svg>
-            </div>
-            <div className='red-social yt'>
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-youtube" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M2 8a4 4 0 0 1 4 -4h12a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-12a4 4 0 0 1 -4 -4v-8z" />
-                <path d="M10 9l5 3l-5 3z" />
-              </svg>
-            </div>
-          </div>
         </footer>
 
       </EntradaContext.Provider>
